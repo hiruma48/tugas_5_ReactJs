e378618b7f75a2861bc20f13e414423955c929a5
tri ariyanto <triyanto129@gmail.com>
2023-07-18 06:05:50 +0700

    tugas 5 reactjs
    mengumpulkan tugas

---
+++ tugas_5_react/src/Lib/DaftarMakanan.js
@@ -0,0 +1,32 @@
+
+
+const DaftarMakanan = [
+    {
+        NamaMakanan: "Nasi Padang",
+        Harga : "20.000",
+        img : "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg"
+    },
+    {
+        NamaMakanan: "sate",
+        Harga : "20.000",
+        img : "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+    },
+    {
+        NamaMakanan: "soto",
+        Harga : "10.000",
+        img : "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png"
+    },
+    {
+        NamaMakanan: "uduk",
+        Harga : "10.000",
+        img : "https://www.dbs.com/iwov-resources/images/newsroom/indonesia/Blog/masakan%20nusantara/nasi%20kentut.png"
+    },
+    {
+        NamaMakanan: "nasi kuning",
+        Harga : "15.000",
+        img : "https://blog.tokowahab.com/wp-content/uploads/2020/03/Resep-Nasi-Kuning-Tumpeng-Mini.jpg "
+    },
+    
+
+];
+export default DaftarMakanan;
